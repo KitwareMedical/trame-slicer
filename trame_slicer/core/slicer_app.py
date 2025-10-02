725392a3701858506970d2b3053ee50f67f5afac
Thibault Pelletier <thibault.pelletier@kitware.com>
2025-10-02 08:25:48 +0200

    feat(slicer_app): module logic registration mechanism
    - Factorize module registration mechanism
    - Fix display node cleanup by using data module logic instead

--- trame_slicer/core/slicer_app.py
+++ trame_slicer/core/slicer_app.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from contextlib import suppress
 from pathlib import Path
 
-import vtk
 from slicer import (
     vtkMRMLColorLogic,
     vtkMRMLCrosshairNode,
@@ -11,19 +11,14 @@ from slicer import (
     vtkMRMLSliceViewDisplayableManagerFactory,
     vtkMRMLThreeDViewDisplayableManagerFactory,
     vtkSlicerApplicationLogic,
+    vtkSlicerDataModuleLogic,
+    vtkSlicerModuleLogic,
     vtkSlicerSegmentationsModuleLogic,
     vtkSlicerSubjectHierarchyModuleLogic,
     vtkSlicerTerminologiesModuleLogic,
     vtkSlicerVolumesLogic,
 )
 from vtkmodules.vtkCommonCore import vtkCollection, vtkOutputWindow
-
-from .display_manager import DisplayManager
-from .io_manager import IOManager
-from .markups_logic import MarkupsLogic
-from .segmentation_editor import SegmentationEditor
-from .view_manager import ViewManager
-from .volume_rendering import VolumeRendering
 
 
 class SlicerApp:
@@ -36,6 +31,11 @@ class SlicerApp:
     def __init__(self, share_directory: str | None = None):
         from trame_slicer.resources import resources_path
 
+        from .display_manager import DisplayManager
+        from .io_manager import IOManager
+        from .segmentation_editor import SegmentationEditor
+        from .view_manager import ViewManager
+
         self.share_directory = Path(share_directory or resources_path())
 
         # Output VTK warnings to console by default
@@ -44,10 +44,6 @@ class SlicerApp:
         vtkOutputWindow.SetInstance(vtk_out)
 
         self.scene = vtkMRMLScene()
-        self.scene.AddObserver(
-            vtkMRMLScene.NodeAboutToBeRemovedEvent,
-            self._remove_attached_displayable_nodes,
-        )
 
         # Add one crosshair to the scene
         # Copied from qSlicerCoreApplication::setMRMLScene
@@ -68,48 +64,12 @@ class SlicerApp:
         vtkMRMLThreeDViewDisplayableManagerFactory.GetInstance().SetMRMLApplicationLogic(self.app_logic)
         vtkMRMLSliceViewDisplayableManagerFactory.GetInstance().SetMRMLApplicationLogic(self.app_logic)
 
-        # Create colors logic
-        self.color_logic = vtkMRMLColorLogic()
-        self.color_logic.SetMRMLApplicationLogic(self.app_logic)
-        self.color_logic.SetMRMLScene(self.scene)
-        self.app_logic.SetModuleLogic("Colors", self.color_logic)
-
-        # Create volume rendering
-        self.volume_rendering = VolumeRendering(self.scene, self.app_logic, self.share_directory.as_posix())
-
-        # Create markups logic
-        self.markups_logic = MarkupsLogic(self.scene, self.app_logic)
-
-        # Initialize volumes logic
-        self.volumes_logic = vtkSlicerVolumesLogic()
-        self.volumes_logic.SetMRMLScene(self.scene)
-        self.volumes_logic.SetMRMLApplicationLogic(self.app_logic)
-        self.app_logic.SetModuleLogic("Volumes", self.volumes_logic)
-
-        # Set up Terminologies logic (needed for subject hierarchy tree view color/terminology selector)
-        self.terminologies_logic = vtkSlicerTerminologiesModuleLogic()
-        self.terminologies_logic.SetModuleShareDirectory(self.share_directory.joinpath("terminologies").as_posix())
-        self.terminologies_logic.SetMRMLScene(self.scene)
-        self.terminologies_logic.SetMRMLApplicationLogic(self.app_logic)
-        self.app_logic.SetModuleLogic("Terminologies", self.terminologies_logic)
-
-        # Set up Segmentation logic
-        self.segmentation_logic = vtkSlicerSegmentationsModuleLogic()
-        self.segmentation_logic.SetMRMLScene(self.scene)
-        self.segmentation_logic.SetMRMLApplicationLogic(self.app_logic)
-        self.app_logic.SetModuleLogic("Segmentation", self.segmentation_logic)
-
-        # Initialize subject hierarchy logic
-        self.sh_module_logic = vtkSlicerSubjectHierarchyModuleLogic()
-        self.sh_module_logic.SetMRMLScene(self.scene)
-        self.sh_module_logic.SetMRMLApplicationLogic(self.app_logic)
+        # Register builtin module logic
+        self._register_builtin_logic()
 
         # Initialize orientation definitions
         patient_right_is_screen_left = True
-        vtkMRMLSliceNode.AddDefaultSliceOrientationPresets(
-            self.scene,
-            patient_right_is_screen_left,
-        )
+        vtkMRMLSliceNode.AddDefaultSliceOrientationPresets(self.scene, patient_right_is_screen_left)
 
         # initialize view manager responsible for creating new views in the app
         self.view_manager = ViewManager(self.scene, self.app_logic)
@@ -118,18 +78,55 @@ class SlicerApp:
         self.display_manager = DisplayManager(self.view_manager, self.volume_rendering)
 
         # Initialize segmentation editor
-        self.segmentation_editor = SegmentationEditor(self.scene, self.segmentation_logic, self.view_manager)
+        self.segmentation_editor = SegmentationEditor(self.scene, self.segmentations_logic, self.view_manager)
 
         # Initialize IO manager
         self.io_manager = IOManager(self.scene, self.app_logic, self.segmentation_editor)
 
-    @vtk.calldata_type(vtk.VTK_OBJECT)
-    def _remove_attached_displayable_nodes(self, scene, _event_id, node):
-        if not scene:
-            return
+    def _register_builtin_logic(self):
+        """
+        Register builtin Slicer application  module logic.
+        """
 
-        if not hasattr(node, "GetNumberOfDisplayNodes"):
-            return
+        from .markups_logic import MarkupsLogic
+        from .volume_rendering import VolumeRendering
 
-        for i_display_node in range(node.GetNumberOfDisplayNodes()):
-            scene.RemoveNode(node.GetNthDisplayNode(i_display_node))
+        self.color_logic = self.register_module_logic(vtkMRMLColorLogic(), logic_name="Colors")
+        self.volume_rendering = VolumeRendering(self)
+        self.markups_logic = MarkupsLogic(self)
+        self.volumes_logic = self.register_module_logic(vtkSlicerVolumesLogic())
+        self.terminologies_logic = self.register_module_logic(
+            vtkSlicerTerminologiesModuleLogic(), share_sub_folder="terminologies"
+        )
+        self.segmentations_logic = self.register_module_logic(vtkSlicerSegmentationsModuleLogic())
+        self.subject_hierarchy_logic = self.register_module_logic(vtkSlicerSubjectHierarchyModuleLogic())
+        self.data_logic = self.register_module_logic(vtkSlicerDataModuleLogic())
+
+    def register_module_logic(self, logic: vtkSlicerModuleLogic, *, logic_name: str = "", share_sub_folder: str = ""):
+        """
+        Configure the input module logic with the application scene, logic and share directory.
+        Registers the logic to the application logic with the given input logic name if any, module name otherwise.
+
+        :param logic: instance of logic to register
+        :param logic_name: name of the logic in the application logic
+        :param share_sub_folder: sub folder of the share directory (if no sub folder, common share directory is used)
+        :return: instance of logic
+        """
+        if not logic_name:
+            logic_name = self._get_default_logic_name(logic)
+
+        # Try to configure share directory before setting the scene / application logic to avoid warning generations
+        share_directory = self.share_directory.joinpath(share_sub_folder) if share_sub_folder else self.share_directory
+        with suppress(AttributeError):
+            logic.SetModuleShareDirectory(share_directory.as_posix())
+
+        logic.SetMRMLScene(self.scene)
+        logic.SetMRMLApplicationLogic(self.app_logic)
+        self.app_logic.SetModuleLogic(logic_name, logic)
+        return logic
+
+    def _get_default_logic_name(self, logic) -> str:
+        logic_name = type(logic).__name__
+        for n in ["vtkSlicer", "Module", "Logic"]:
+            logic_name = logic_name.replace(n, "")
+        return logic_name
